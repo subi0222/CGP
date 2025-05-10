1f1f451524e70885c310739e9d6a4ccaad51a343
brian70304 <brian70304@gmail.com>
2025-05-10 17:40:17 +0900

    의사 애니메이션 고침

--- Assets/NPC/JS_NPC/Scripts/NPCDoctorMovement.cs
+++ Assets/NPC/JS_NPC/Scripts/NPCDoctorMovement.cs
@@ -8,7 +8,6 @@ public class NPCDoctorMovement : MonoBehaviour
     private NavMeshAgent agent;
 
     // 플레이어에게 보일경우 멈추는 코드 추가
-    private bool hasBeenSpotted = false; // 한 번이라도 플레이어에게 보였나
     private bool isSeenByPlayer = false; // 현재 보여지고 있나
     private Vector3 savedDestination;    // Nav Mesh 목적지 저장
     // 여기까지 추가함
@@ -30,24 +29,27 @@ public class NPCDoctorMovement : MonoBehaviour
             Vector3 localDir = transform.InverseTransformDirection(velocity.normalized);
             animator.SetFloat("X", localDir.x, 0.1f, Time.deltaTime);
             animator.SetFloat("Y", localDir.z, 0.1f, Time.deltaTime);
+            // 의사 정지시 이상한 포즈 해결 위한 코드 추가
+            animator.speed = 1f;
+            // 여기까지 추가함
             Vector3 AgentVelocity = agent.velocity;
         }
         else // 정지 상태
         {
-            animator.SetFloat("X", 0f, 0.1f, Time.deltaTime);
-            animator.SetFloat("Y", 0f, 0.1f, Time.deltaTime);
+            // 의사 정지시 이상한 포즈 해결 위한 코드 변경
+            if (!animator.GetBool("GrabPlayer")) // 그랩상태가 아니면 애니메이션 정지
+            {
+                animator.Play(animator.GetCurrentAnimatorStateInfo(0).fullPathHash, -1, animator.GetCurrentAnimatorStateInfo(0).normalizedTime);
+                animator.speed = 0f;
+            }
+            else // 그랩상태면 애니메이션 계속
+            {
+                animator.speed = 1f;
+            }
+            // 여기까지 변경함
         }
 
         // 플레이어에게 보일경우 멈추는 코드 추가
-        if (!hasBeenSpotted)
-        {
-            // 플레이어에게 보여진적 없으면 움직이지 않음
-            agent.isStopped = true;
-            savedDestination = agent.destination;
-            Debug.Log("doctor has not Been Spotted");
-            return;
-        }
-
         if (isSeenByPlayer)
         {
             // 플레이어에게 보여지고 있으면 움직이지 않음
@@ -79,11 +81,6 @@ public class NPCDoctorMovement : MonoBehaviour
     // 플레이어에게 보일경우 멈추는 코드 추가
     public void OnSpottedByPlayer()
     {
-        if (!hasBeenSpotted)
-        {
-            hasBeenSpotted = true;
-            Debug.Log("doctor has Been Spotted");
-        }
         isSeenByPlayer = true;
     }
 
